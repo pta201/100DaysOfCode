a48382ce3abf6dee6251dc7037b98d1b7403d058
pta208 <pta20801@gmail.com>
2022-03-31 08:42:56 +0700

    Adding Section11 first exercise

---
+++ Section11_Exercises/exercise.js
@@ -0,0 +1,45 @@
+// This is how a comment is added to JavaScript
+// Comments are not executed - they are just there to provide extra
+// information to you or other developers
+
+// Exercise Time!
+
+// 1) Create three new variables:
+//    - A variable that stores the name of an online course of your choice
+let course = "100 Days Of Code";
+//    - A variable that stores the price of that course
+let price = 13.99;
+//    - A variable that stores the three main goals that you have, when taking this course
+let goals = [
+  "Learning Front-end web development",
+  "Try out fullstack",
+  "Make a demo",
+];
+// 2) Output ("alert") the three variable values
+alert(course);
+alert(price);
+alert(goals);
+// 3) Try "grouping" the three variables together and still output their values thereafter
+let grouped = {
+  course: "100 Days Of Code",
+  price: "13.99",
+  goals: [
+    "Learning Front-end web development",
+    "Try out fullstack",
+    "Make a demo",
+  ],
+};
+// 4) Also output the second element in your "main goals" variable
+console.log(goals[1]);
+// 5) Add a custom command that does the following:
+//    - Use your "main goals" variable and access an element by its identifier
+//    - The concrete identifier value should be dynamic / flexible
+//      (i.e. the command can be executed for different identifier)
+//    - The "main goals" variable should also be dynamic: The command should work
+//      with ANY list of values
+//    - The custom command should provide the accessed value (i.e. the list element)
+function getGoal(list, index) {
+  return list[identifier];
+}
+// 6) Execute your custom command from (5) and output ("alert") the result
+alert(getGoal(goals, index));
